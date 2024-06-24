d0d03388b3cdcf919fe20ee6335d53df29c6e492
tqbed <66844373+tqbed@users.noreply.github.com>
2024-06-24 11:10:04 -0400

    Finish R logo and add random circle function

--- project.js
+++ project.js
@@ -10,32 +10,84 @@ const height = 125;
 setDocDimensions(width, height);
 
 // Store drawings here
-const finalLines = []
 
+const finalLines = []
 const t = new bt.Turtle();
 
-t.jump([50, 10]);
+// Random variables for gear + circle
+
+const xstCircle = bt.randIntInRange(55, 75)
+const xstGear = bt.randIntInRange(0, 10)
+const ystCircle = bt.randIntInRange(45, 65)
+const ystGear = bt.randIntInRange(0, 10)
+const cRadius = bt.randIntInRange(10, 20)
+
+// Draw the starting circle 
+
+// Function to draw a circle
+
+function drawCircle(startX, startY, radius) {
+  t.jump([startX, startY - radius]); 
+  t.down();
+  t.arc(360, radius); 
+}
+
+drawCircle(xstCircle, ystCircle, cRadius)
+
+
+// Draw the R (I know this code is horrifying) 
+
+t.jump([90, 10]);
 
 t.setAngle(90);
 
 t.down();
-t.forward(20);
-
+t.forward(13);
 t.right(90);
-t.forward(15);
+t.forward(5);
+t.left(90);
+t.forward(6);
+t.left(90);
+t.forward(5);
+t.right(90);
+t.forward(1);
 
+
+t.right(180);
+t.up();
+t.forward(4);
+t.left(90);
+t.forward(8.25);
+t.down();
+
+t.forward(4.25);
 t.arc(180, 7.5);
 
-t.forward(15);
+t.forward(12.5);
 
 t.right(-90);
-t.forward(15);
+t.forward(11);
 
-t.jump([50, 30]);
+t.jump([98.25, 26]);
 
-t.setAngle(315);
-t.forward(27.5);
+t.setAngle(310);
+t.forward(21);
 
+t.jump([90, 10]);
+t.setAngle(180);
+t.forward(5);
+
+t.right(90);
+t.forward(13);
+
+t.left(90);
+t.forward(5);
+t.right(90);
+t.forward(6);
+t.right(90);
+t.forward(5);
+t.left(90);
+
+// Draw the finished product 
 drawLines(t.lines());
-
 drawLines(finalLines);
